0aea419af4e51bf9b77b69f62661f39f79214f7b
Abraham Andrews <129630636+Abe-Andrews@users.noreply.github.com>
2026-06-30 19:55:49 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        string_1_map = {}
+        for letter in s:
+            if letter in string_1_map:
+                string_1_map[letter] += 1
+            else:
+                string_1_map[letter] = 1
+
+        string_2_map = {}
+        for letter in t:
+            if letter in string_2_map:
+                string_2_map[letter] += 1
+            else:
+                string_2_map[letter] = 1        
+
+        return string_1_map == string_2_map
+   
+
+        
